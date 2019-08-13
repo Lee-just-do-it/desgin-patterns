ea5f9f31dd64f46968ddf383bc24b17e4b8fa20d
lee <yanmo@uoko.com>
2019-08-13 19:27:56 +0800

    设计模式 初始化

---
+++ src/main/java/com/desgin/patterns/DesginPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.desgin.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesginPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesginPatternsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/desgin/patterns/DesginPatternsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.desgin.patterns;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DesginPatternsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
