0a09c5816f3c70220789f40bf8ea941307ef8edc
lee <yanmo@uoko.com>
2019-08-14 13:53:06 +0800

    工厂模式-简单示例

---
+++ src/main/java/com/desgin/patterns/factory/HouseBuildAbstractFactory.java
@@ -0,0 +1,14 @@
+package com.desgin.patterns.factory;
+
+/**
+ * @author 木子Lee
+ * @desc 抽象工厂
+ * @date 2019/8/13 13：30
+ * @since 1.0
+ */
+public class HouseBuildAbstractFactory extends AbstractBuildFactory {
+    @Override
+    public Build build() {
+        return new HouseBuild();
+    }
+}
